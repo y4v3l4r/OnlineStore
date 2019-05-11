693ef2a433455227f7ed60ceaec20b6650aa7cd3
Student <student@Admins-MacBook-Air-2.local>
2019-05-11 15:47:30 -0700

    Initial commit

---
+++ app/src/main/java/com/teamedge/android/onlinestore/MiniStore.java
@@ -0,0 +1,13 @@
+package com.teamedge.android.onlinestore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiniStore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mini_store);
+    }
+}
